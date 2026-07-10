c4ef9eade6fc3f08c9f1a542b793a3881bfc7e40
deeptirao1210 <deeptirao1210@gmail.com>
2026-07-10 10:44:39 +0530

    Service Registry

---
+++ backend/service-registry/src/test/java/com/deepti/ecommerce/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepti.ecommerce.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
